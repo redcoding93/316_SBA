17041d576091e85bae87940bdf26c844af2f591b
Vingram <vingram15@gmail.com>
2024-01-31 14:24:33 -0800

    add iteration and radio buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
- let signup = document.querySelector(".signup");
  let login = document.querySelector(".login");
 
  
@@ -12,6 +11,7 @@
 //     formSection.classList.remove("form-section-move");
 // });
 
+//Username validation
 function validateForm() {
     var x = document.forms["myForm"]["fname"].value;
     if (x == "" || x == null) {
@@ -20,10 +20,52 @@ function validateForm() {
     }
   }
 
+  //BOM alert when submitting account creation
+
+  let email = document.querySelector(".email");
   function myFunction() {
     alert("Congrats on becoming a member!");
   }
 
+//   function myFunction() {
+//     alert("Congrats on becoming a member!");
+//   }
+
+//BOM alert after login form submit
+
   function myFunction2() {
     alert("You are now logged in!");
   }
+
+  //Password verification
+  function checkPassword(form) { 
+    password1 = form.password1.value; 
+    password2 = form.password2.value; 
+
+    // If password not entered 
+    if (password1 == '') 
+        alert ("Please enter Password"); 
+          
+    // If confirm password not entered 
+    else if (password2 == '') 
+        alert ("Please enter confirm password"); 
+          
+    // If Not same return False.     
+    else if (password1 != password2) { 
+        alert ("\nPassword did not match: Please try again...") 
+        return false; 
+    } 
+
+    // If same return True. 
+    else{ 
+        alert("Password Match: Welcome to GeeksforGeeks!") 
+        return true; 
+    } 
+} 
+
+//Iterate over a collection of elements to accomplish some task.
+const inputs = document.getElementsByTagName("input");
+
+for (let input of inputs) {
+  console.log(input);
+}
