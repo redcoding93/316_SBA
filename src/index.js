6ba5b4bede5b4723f8b6b6c056d68f3d51101aab
Vingram <vingram15@gmail.com>
2024-01-31 10:48:51 -0800

    update colors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-let signup = document.querySelector(".signup");
-let login = document.querySelector(".login");
-let slider = document.querySelector(".slider");
-let formSection = document.querySelector(".form-section");
+ let signup = document.querySelector(".signup");
+ let login = document.querySelector(".login");
+
  
-signup.addEventListener("click", () => {
-    slider.classList.add("moveslider");
-    formSection.classList.add("form-section-move");
-});
+// signup.addEventListener("click", () => {
+//     slider.classList.add("moveslider");
+//     formSection.classList.add("form-section-move");
+// });
  
-login.addEventListener("click", () => {
-    slider.classList.remove("moveslider");
-    formSection.classList.remove("form-section-move");
-});
+// login.addEventListener("click", () => {
+//     slider.classList.remove("moveslider");
+//     formSection.classList.remove("form-section-move");
+// });
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "" || x == null) {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+  function myFunction() {
+    alert("Congrats on becoming a member!");
+  }
+
+  function myFunction2() {
+    alert("You are now logged in!");
+  }
