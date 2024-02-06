cd691484783e976ba359b68186d43fcb165cf9cd
Vingram <vingram15@gmail.com>
2024-02-05 19:01:23 -0800

    update text content and user input section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 
 
 
+//Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+//Click on hello text to see it change
+const changeText = document.querySelector("#change-text");
 
-
+changeText.addEventListener("click", function() {
+  changeText.textContent = "Bye! We can't wait to see you again next time";
+});
 //Include at least one form and/or input with DOM event-based validation. 
 //required the field to be filled out, must be 8 characters; no more than 15 characters; alets for all three if input is invalid
 function verifyPassword() {  
